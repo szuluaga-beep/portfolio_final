16cf49e0df697449eea2ef6fbfab53ccc20553c2
szuluaga-beep <steven000069@hotmail.com>
2024-05-18 20:57:18 -0500

    creando menu de navegación

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,121 @@
+'use client'
+import React, { FC, ReactNode, useEffect, useState } from 'react'
+import { useTheme } from "next-themes";
+
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, Switch } from '@nextui-org/react';
+import { MoonIcon, SunIcon } from '@heroicons/react/16/solid';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'
+
+const menuItems = [
+    {
+        id: 1,
+        name: "Home page",
+        href: '/'
+    },
+    {
+        id: 2,
+        name: "About me",
+        href: '/about'
+    },
+    {
+        id: 2,
+        name: "Portfolio",
+        href: '/portfolio'
+    },
+
+];
+
+
+const CustomNavbar = () => {
+
+    const pathname = usePathname()
+    const { theme, setTheme } = useTheme()
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [mounted, setMounted] = useState(false)
+    const [isSelected, setIsSelected] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) return null
+
+    const toggleSwitch = () => {
+        setIsSelected((prev) => !prev)
+        setTheme(isSelected ? 'dark' : 'light')
+    }
+
+    return (
+        <>
+
+            <Navbar onMenuOpenChange={setIsMenuOpen}>
+                <NavbarContent>
+                    <NavbarMenuToggle
+                        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                        className="sm:hidden"
+                    />
+                    <NavbarBrand>
+
+                        <p className="font-bold text-inherit">STEVENDEV</p>
+                    </NavbarBrand>
+                </NavbarContent>
+
+                <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                    {
+                        menuItems.map((item) => (
+                            <NavbarMenuItem key={item.id}>
+                                <Link
+                                    // color='primary'
+                                    className={`link ${pathname === item.href ? 'text-blue-700 font-bold' : ''}`}
+                                    href={item.href}
+                                // size="lg"
+                                >
+                                    {item.name}
+                                </Link>
+                            </NavbarMenuItem>
+                        ))
+                    }
+                </NavbarContent>
+
+                <Switch
+                    isSelected={isSelected}
+                    onValueChange={toggleSwitch}
+                    color='success'
+                    size="lg"
+                    startContent={<SunIcon />}
+                    endContent={<MoonIcon />}
+                >
+
+                </Switch>
+
+                {/* <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="#">Login</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="#" variant="flat">
+                        Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent> */}
+                <NavbarMenu>
+                    {menuItems.map((item) => (
+                        <NavbarMenuItem key={item.id}>
+                            <Link
+                                onClick={() => setIsMenuOpen(true)}
+                                className="w-full"
+                                href={item.href}
+                            // size="lg"
+                            >
+                                {item.name}
+                            </Link>
+                        </NavbarMenuItem>
+                    ))}
+                </NavbarMenu>
+            </Navbar>
+        </>
+    )
+}
+
+export default CustomNavbar
