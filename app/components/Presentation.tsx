d67367266206454e8fb834ab21f1354111bd3d1e
szuluaga-beep <steven000069@hotmail.com>
2024-05-20 12:29:38 -0500

    ajustes estilos presentation

--- app/components/Presentation.tsx
+++ app/components/Presentation.tsx
@@ -24,10 +24,10 @@ const Presentation = () => {
     }, []);
     return (
 
-        <div className='mt-2 px-10 py-5 flex shadow-2xl sm:grid-cols-1 md:grid-cols-2'>
-            <div className='flex flex-col justify-start items-start rounded-lg px-8 py-10 gap-4 sm:w-full md:w-3/4  mt-2'>
+        <div className='mt-2 px-10 flex justify-center items-center py-5 w-full shadow-2xl grid-cols-2'>
+            <div className='flex flex-col justify-start items-center sm:items-start rounded-lg px-8 py-10 gap-4 mt-2 w-full xl:w-3/4'>
                 <Image
-                    className="w-52 h-52 rounded-3xl -rotate-12 ml-3 object-cover hover:animate-spin"
+                    className="w-52 h-52 rounded-3xl -rotate-12 ml-3 object-cover mb-3 hover:animate-spin"
                     src={profilePic}
                     alt="Picture of the author"
                 // width={500} automatically provided
@@ -36,12 +36,12 @@ const Presentation = () => {
                 // placeholder="blur" // Optional blur-up while loading
                 />
 
-                <h1 className='text-4xl font-bold'>Hi, I am Steven - Developer and Engineer</h1>
                 <div className='flex flex-col'>
+                    <h1 className='text-4xl font-bold mb-3'>Hi, I am Steven - Developer and Engineer</h1>
 
                     <span className='sm:text-2xl md:text-4xl leading-snug'>
                         I enjoy creating products from
-                        scratch and improve existing ones.<br/>
+                        scratch and improve existing ones.<br />
                         In simple terms, I develop
                     </span>
                     <motion.div
@@ -60,18 +60,19 @@ const Presentation = () => {
                 </div>
                 <div className='flex gap-2 justify-center items-center'>
 
-                    <a href="https://api.whatsapp.com/send/?phone=573137896663" className=" hover:scale-110 h-10 px-6 font-semibold rounded-full bg-violet-600 text-white flex items-center" target='_blank'>
+                    <a href="https://api.whatsapp.com/send/?phone=573137896663" className="px-3 hover:scale-110 h-10 font-semibold rounded-full bg-violet-600 text-white flex items-center" target='_blank'>
                         Let&apos;s talk
                     </a>
 
                     <Chip className=' p-4' color="success" variant="dot">Open for opportunities</Chip>
                 </div>
             </div>
-            <div className='sm:hidden md:block w-1/4 left-5 gap-2 flex flex-wrap p-5 relative'>
-                <MobilePicture style="sm:hidden md:block w-3/5 absolute top-0 right-0 stroke-2" />
-                <WebPicture style="sm:hidden md:block w-3/5 absolute bottom-0 left-0 stroke-2" />
+            <div className='hidden xl:block w-1/4'>
 
+                <MobilePicture style="mt-5" />
             </div>
+
+
 
         </div>
 
